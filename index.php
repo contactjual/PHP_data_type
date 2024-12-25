f3c9dd472b444200b93e4d4fe8888b2383e9749f
Jual Rana <contactjual@gmail.com>
2024-12-25 17:18:18 +0600

    data ytpe

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <?php 
 
-// data type
-
 // int
 $x = null;
 $x = 100;
 echo $x;
+
+/* data type------------------------------
+
+String
+Integer
+Float (floating point numbers - also called double)
+Boolean
+Array
+Object
+NULL
+Resource 
+*/
+
+
+// Array [The PHP var_dump() function returns the data type and value]
+
+$my_array = array("pent", "longi", "gamcha", "shirt");
+// var_dump($my_array); // or
+echo var_dump($my_array);
+
+
+
+
+
+// Resource
+
+The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP
+
 
 
 
