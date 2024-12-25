3d9d8a2daa2bb29afd80091b7ac223df53248b06
Jual Rana <contactjual@gmail.com>
2024-12-25 17:28:17 +0600

    array data type

--- index.php
+++ index.php
@@ -24,14 +24,21 @@ $my_array = array("pent", "longi", "gamcha", "shirt");
 // var_dump($my_array); // or
 echo var_dump($my_array);
 
+$my_arrays = array1 ("boi", "khata", "kolom", array2 ("bois", "khatas", "koloms"));
+echo var_dump($my_arrays);
+
+
+
+
+
+
 
 
 
 
 // Resource
 
-The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP
-
+// The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP
 
 
 
